1ee3150e167975b9847964f71e08cad1b686707c
MuchuanTang <147974093+MuchuanTang@users.noreply.github.com>
2024-10-02 18:05:11 +0800

    zhushi

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express')//Import the express module
+const app = express()//Create an instance of an express application
 
 app.all('*', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -7,15 +7,15 @@ app.all('*', function (req, res, next) {
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 	res.header("X-Powered-By", ' 3.2.1')
 	res.header("Content-Type", "application/json;charset=utf-8");
-	next();
+	next();//Pass control to the next middleware in the stack
 });
 
 app.use(express.json())
 
 let user = require('./api.js');
-app.use(user);
+app.use(user); //Use the user API as a route handler
 
-module.exports = app
+module.exports = app //Export the app for use in other modules
 const port =4001
 app.listen(port, () => {
 	console.log(`app is running at http://localhost:${port}`)
