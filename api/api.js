483b27eb51c92a2440e82e694ba172241b1417dc
MuchuanTang <147974093+MuchuanTang@users.noreply.github.com>
2024-10-02 18:27:07 +0800

    zhushi

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 
 var app = require("express").Router();
 const DBPool = require('./crowdfunding_db.js')
+//Get a list of all fundraisers
 
 app.get('/getFundraisersList', (req, res) => {
     let searchSql = 'SELECT * from `FUNDRAISER`'
@@ -8,7 +9,7 @@ app.get('/getFundraisersList', (req, res) => {
         let result = results.results
         res.send(result);
     })
-});
+});     //Get a list of active fundraisers
 app.get("/Fundraisers/getActiveFundraisers", (req, res) => {
     let param = req.query
     let searchSql = 'SELECT * from `FUNDRAISER` WHERE ACTIVE = ?'
@@ -16,7 +17,7 @@ app.get("/Fundraisers/getActiveFundraisers", (req, res) => {
         let result = results.results
         res.send(result);
     })
-});
+});    //Get one fundraiser by category ID
 app.get("/Fundraisers/getOneFundraiser", (req, res) => {
     let param = req.query
     if (param.CATEGORYID) {
@@ -31,7 +32,7 @@ app.get("/Fundraisers/getOneFundraiser", (req, res) => {
             let result = results.results
             res.send(result);
         })
-    }
+    }       //Get details of a fundraiser by its ID
 
 });
 app.get("/Fundraisers/Details", (req, res) => {
@@ -39,7 +40,7 @@ app.get("/Fundraisers/Details", (req, res) => {
     let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISERID = ?'
     DBPool.query(searchSql, [param.FUNDRAISERID], (results) => {
         let result = results.results
-        res.send(result);
+        res.send(result);       //Get a list of all categories
     })
 });
 app.get("/Fundraisers/List", (req, res) => {
@@ -47,7 +48,7 @@ app.get("/Fundraisers/List", (req, res) => {
     DBPool.query(searchSql, [], (results) => {
         let result = results.results
         res.send(result);
-    })
+    })//Export the router
 });
 
 
